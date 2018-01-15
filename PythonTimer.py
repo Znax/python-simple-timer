81261ed16bedc9fc58a252c47a07db3316bb9f4a
Patrick Salomon <patrick.salomon@cossette.com>
2018-01-15 10:39:39 -0500

    improve timer and add process timer

--- PythonTimer.py
+++ PythonTimer.py
@@ -4,28 +4,37 @@ import time
 class PythonTimer:
 
     def __init__(self):
-        self.all_times = {}
-        self.cumulated_times = {}
-        self.cumulated_count = {}
+        self.perf_counter = {}
+        self.process_time = {}
+        self.cumulated_perf_counter = {}
+        self.cumulated_process_time = {}
+        self.count = {}
 
-    def reset_timer(self):
-        self.all_times = {}
-        self.cumulated_times = {}
-        self.cumulated_count = {}
+    def start(self, name):
+        self.perf_counter[name] = time.perf_counter()
+        self.process_time[name] = time.process_time()
 
-    def start_timer(self, name):
-        self.all_times[name] = time.time()
+    def stop(self, name):
+        elapsed_perf_counter = (time.perf_counter() - self.perf_counter[name]) * 1000
+        elapsed_process_time = (time.process_time() - self.process_time[name]) * 1000
+        if name not in self.cumulated_perf_counter:
+            self.cumulated_perf_counter[name] = 0
+            self.cumulated_process_time[name] = 0
+            self.count[name] = 0
+        self.cumulated_perf_counter[name] += elapsed_perf_counter
+        self.cumulated_process_time[name] += elapsed_process_time
+        self.count[name] += 1
 
-    def stop_timer(self, name):
-        elapsed = time.time() - self.all_times[name]
-        if name not in self.cumulated_times:
-            self.cumulated_times[name] = 0
-            self.cumulated_count[name] = 0
-        self.cumulated_times[name] += elapsed
-        self.cumulated_count[name] += 1
+    def get_perf_counter_ms(self):
+        return self.cumulated_perf_counter
 
-    def get_cumulated_times(self):
-        return self.cumulated_times
+    def get_process_time_ms(self):
+        return self.cumulated_process_time
 
-    def get_cumulated_count(self):
-        return self.cumulated_count
+    def get_count(self):
+        return self.count
+
+    def reset(self):
+        self.perf_counter = {}
+        self.cumulated_perf_counter = {}
+        self.count = {}
--- example.py
+++ example.py
@@ -6,17 +6,24 @@ from PythonTimer import PythonTimer
 my_timer = PythonTimer()
 
 # monitor only one line
-my_timer.start_timer('one')
+my_timer.start('one')
 time.sleep(0.2)
-my_timer.stop_timer('one')
+my_timer.stop('one')
 
 # if multiple lines, will cumulate the result
 for loop in [0.1, 0.2, 0.3]:
-    my_timer.start_timer('loop')
+    my_timer.start('loop')
     print(loop)
     time.sleep(loop)
-    my_timer.stop_timer('loop')
+    my_timer.stop('loop')
+
+# if multiple lines, will cumulate the result
+for num in range(50000):
+    my_timer.start('range')
+    print(num)
+    my_timer.stop('range')
 
 
-print(my_timer.get_cumulated_times())
-print(my_timer.get_cumulated_count())
+print(my_timer.get_perf_counter_ms())
+print(my_timer.get_process_time_ms())
+print(my_timer.get_count())
